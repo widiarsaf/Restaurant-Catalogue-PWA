bfc7df8c2a289c8c3b30130c4cff35c1c5f340ae
widiarsaf <wretasafitri33@gmail.com>
2021-07-09 22:15:27 +0700

    implement chace API to service worker

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium',
+  CACHE_NAME: `resto-explorer-${new Date().toISOString()}`,
 };
 
 export default CONFIG;
